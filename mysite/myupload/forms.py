f95d6f5c825d0a43eb3b65ab7966e24174800933
aidken <aidken+github@gmail.com>
2017-09-02 13:33:53 +0900

    Update forms.py. Forms are instantiated from this. Probably.

--- mysite/myupload/forms.py
+++ mysite/myupload/forms.py
@@ -5,5 +5,5 @@ from django import forms
 class UploadFileForm(forms.Form):
     # title         = forms.CharField(max_length=50)
     file            = forms.FileField(label='Select a file.')
-    uploda_datetime = forms.CharField(label='Enter date... for now.')
+    upload_datetime = forms.CharField(label='Enter date manually... for now.')
     comment         = forms.CharField(label='Comment.', max_length=100)
